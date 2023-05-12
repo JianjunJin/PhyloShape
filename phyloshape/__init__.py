b2aa48a72d8ded15fe05b8f4806098975fb58d43
Deren <de2356@columbia.edu>
2023-05-12 15:10:44 -0400

    new mapper push

--- phyloshape/__init__.py
+++ phyloshape/__init__.py
@@ -2,18 +2,18 @@
 
 """PhyloShape
 """
-from phyloshape.shape import \
-    Vertices, \
-    Faces, \
-    Shape, \
-    ShapeAlignment, \
-    VertexVectorMapper
-from phyloshape.phylo import \
-    PhyloShape, \
-    Brownian
-from phyloshape.utils.src.logger_setup import \
-    set_log_level
 
+from phyloshape.shape import (
+    Vertices,
+    Faces,
+    Shape,
+    ShapeAlignment,
+    VertexVectorMapper
+)
+from phyloshape.phylo import PhyloShape, Brownian
+from phyloshape.utils.src.logger_setup import set_log_level
+import phyloshape.io
+import phyloshape.data 
 
 __version__ = "0.0.1"
 __author__ = "JianJun Jin"
---
+++ phyloshape/io/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+"""I/O opreations for reading/writing 3D data files.
+
+"""
+
+from phyloshape.io.src.parse import *
